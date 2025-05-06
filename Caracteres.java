758b59b856303c3eef6474b3b8c60356b16bcef6
EmmanuelAlvarezGiles <20243rd056@utez.edu.mx>
2025-05-06 12:20:45 -0600

    Elimine la impresion en pantalla

--- Caracteres.java
+++ Caracteres.java
@@ -4,8 +4,6 @@ public class Caracteres{
         char simbolo = '@';
         char numeroChar = '9';
 
-        System.out.println("letra = " + letra);
-        System.out.println("simbolo = " + simbolo);
-        System.out.println("numeroChar = " + numeroChar);
+        
     }
 }
