7eeb39ab6a3dfd753dd709c7c1a5f15c8186342d
EmmanuelAlvarezGiles <20243rd056@utez.edu.mx>
2025-05-06 12:13:15 -0600

    Imprimir en pantalla

--- Caracteres.java
+++ Caracteres.java
@@ -3,5 +3,9 @@ public class Caracteres{
         char letra = 'E';
         char simbolo = '@';
         char numeroChar = '9';
+
+        System.out.println("letra = " + letra);
+        System.out.println("simbolo = " + simbolo);
+        System.out.println("numeroChar = " + numeroChar);
     }
 }
